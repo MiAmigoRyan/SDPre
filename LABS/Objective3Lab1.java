ddb22f14bb62dc0c47d3c5b555d822b979ff39eb
allez_ryan_venga <arment.ryan@gmail.com>
2023-02-21 15:48:17 -0700

    finished Objective3Lab1

---
+++ LABS/Objective3Lab1.java
@@ -0,0 +1,10 @@
+public class Objective3Lab1 {
+	public static void main(String[] args){
+		double num1, num2, num3, average;
+		num1 = 1;
+		num2 = 50;
+		num3 = 100;
+		average = (num1+num2+num3)/3;
+		System.out.println(average);
+	}
+}
