dea53f47d9dfbba89924278e69518e14ec08c51e
allez_ryan_venga <arment.ryan@gmail.com>
2023-02-21 15:18:46 -0700

    finished Objective2Lab2

---
+++ LABS/Objective2Lab2.java
@@ -0,0 +1,8 @@
+public class Objective2Lab2 {
+	public static void main(String[] args) {
+		String name = "Ryan Arment";
+		String age = "37";
+		String stateBorn = "Texas";
+		System.out.println("Name: "+name+" Age: "+age+" State: "+stateBorn);
+	}
+}
