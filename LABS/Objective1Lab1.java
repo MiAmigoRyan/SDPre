dc56871b3ccd18dfefcdbc805a8f9fbd0a242b5d
allez_ryan_venga <arment.ryan@gmail.com>
2023-02-21 14:05:23 -0700

    finished Ojective1Lab1.java

---
+++ LABS/Objective1Lab1.java
@@ -0,0 +1,7 @@
+public class Objective1Lab1 {
+// all code is contained between these curly braces
+  public static void main(String[] args){
+  	//the above line is a method declaration
+  	System.out.println("Hello, Skill Distillery");
+  }
+}
