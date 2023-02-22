b7d9c55cdc0d9fb64c08f3e2ac56ffc09bf2bd09
allez_ryan_venga <arment.ryan@gmail.com>
2023-02-22 14:48:16 -0700

    finished Objective7Lab1

---
+++ LABS/Objective7Lab1.java
@@ -0,0 +1,10 @@
+public class Objective7Lab1 {
+	public static void main (String[] args){
+		int counter = 10;
+
+		while (counter > 0){
+			int counterDown = counter--;
+			System.out.println(counterDown);
+		}
+	}
+}
