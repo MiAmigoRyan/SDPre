f1425627990d3257516b3a3bfda227aace3c68fb
allez_ryan_venga <arment.ryan@gmail.com>
2023-02-22 16:31:11 -0700

    finished Objective7Lab5, edited the starter code to solve

---
+++ LABS/Objective7Lab5.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class Objective7Lab5 {
+  public static void main(String[] args) {
+    Scanner kb = new Scanner(System.in);
+    int selection;
+    int i = 0;
+
+    while (i==0) {
+      System.out.println("_____Menu_____");
+      System.out.println("1: Say Hello");
+      System.out.println("2: List My favorite foods");
+      System.out.println("3: Exit");
+      System.out.println();
+
+      selection = kb.nextInt();
+
+      switch (selection){
+      case 1:
+      	System.out.println("Hello Human");
+      	break;
+      case 2:
+      	System.out.println("Tacos, Pizza, Ramen");
+      	break;
+      case 3:
+      	System.out.println("Goodbye!");
+      	i++;
+      	break;
+      default:
+      	System.out.println("Sorry, that is not a valid option, please try again.");
+      }
+    } 
+  } 
+}
+/* EXPECTE RESULT:
+      _____Menu_____
+		1: Say Hello
+		2: List My favorite foods
+		3: Exit
+
+		1
+		Hello Human
+		_____Menu_____
+		1: Say Hello
+		2: List My favorite foods
+		3: Exit
+
+		2
+		Apples, Bananas, Coconuts
+		_____Menu_____
+		1: Say Hello
+		2: List My favorite foods
+		3: Exit
+
+		3
+		Goodbye
+      */
