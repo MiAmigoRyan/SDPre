a9603a63410cfc1c16a7ee40b5756855bc336fe9
allez_ryan_venga <arment.ryan@gmail.com>
2023-02-22 17:10:15 -0700

    finished Objective8Lab1

---
+++ LABS/Objective8Lab1.java
@@ -0,0 +1,12 @@
+public class Objective8Lab1 {
+	public static void main(String[] args){
+		//Increment by 1, from 0,to 10
+		for (int i=0; i<=10; i++){
+		System.out.println(i);
+	}
+		//Decrement by 1, to 0, from 10
+	for (int j=10; j>=0; --j){
+		System.out.println(j);	
+	}
+	}
+}
